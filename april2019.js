217b7819251ceac3e314cddf01c66cf5ffe9fb34
Matt Russo <matthew.russo91@gmail.com>
2019-04-17 14:25:55 -0400

    add 'Find the Odd Int' solution (6kyu)

--- april2019.js
+++ april2019.js
@@ -2,3 +2,14 @@
 /* Write a function named repeater() that takes two arguments (a string and a number), 
 and returns a new string where the input string is repeated that many times. */
 const repeater = (string, n) => string.repeat(n);
+
+// WED 4/17/19
+// Find the integer in an array that appears an odd number of times
+const findOdd = ints => {
+  let obj = {};
+  ints.forEach(el => obj[el] ? obj[el]++ : obj[el] = 1);
+  
+  for(prop in obj) {
+    if(obj[prop] % 2 !== 0) return Number(prop);
+  }
+}
