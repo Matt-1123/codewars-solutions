3b5505f026d63d924cd280705a4472481317fa31
Matt Russo <matthew.russo91@gmail.com>
2019-12-31 10:34:28 -0500

    12/31 Java and JS solutions

--- codewars-dec19.js
+++ codewars-dec19.js
@@ -26,3 +26,20 @@ class Kata {
       return x.replaceAll("\\s+","");
   }
 }
+
+// Tues Dec 31, 2019
+// Is x dividible by x and y?
+// JS:
+function isDivisible(n, x, y) {
+  if(n % x == 0 && n % y == 0){
+    return true;
+  } else return false; 
+}
+// Java:
+public class DivisibleNb {
+	public static boolean isDivisible(long n, long x, long y) {
+		if(n % x == 0 && n % y == 0){
+      return true;
+    } else return false;
+	}
+}
