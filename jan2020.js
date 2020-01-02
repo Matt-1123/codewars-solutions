503eb5d7d5011f7282172f9ce404f32e2e623d5b
Matt Russo <matthew.russo91@gmail.com>
2020-01-01 21:10:41 -0500

    add jan 2020 file

---
+++ jan2020.js
@@ -0,0 +1,10 @@
+// Wed Jan 1, 2020
+// Find the index of the missing vowel from a string where a=0, e=1, etc.
+JS
+function absentVowel(x){
+  if(x.includes("a") == false && x.includes("A") == false){ return 0; }
+  else if(x.includes("e") == false && x.includes("E") == false){ return 1; }
+  else if(x.includes("i") == false && x.includes("I") == false){ return 2; }
+  else if(x.includes("o") == false && x.includes("O") == false){ return 3; }
+  else if(x.includes("u") == false && x.includes("U") == false){ return 4; }
+}
