3b61c04eab7f56eb34033bd6ec321e2171b96c4e
Matt Russo <matthew.russo91@gmail.com>
2020-01-02 22:18:04 -0500

    jan 2 solution

--- jan2020.js
+++ jan2020.js
@@ -1,6 +1,6 @@
 // Wed Jan 1, 2020
 // Find the index of the missing vowel from a string where a=0, e=1, etc.
-JS
+// JS:
 function absentVowel(x){
   if(x.includes("a") == false && x.includes("A") == false){ return 0; }
   else if(x.includes("e") == false && x.includes("E") == false){ return 1; }
@@ -8,3 +8,13 @@ function absentVowel(x){
   else if(x.includes("o") == false && x.includes("O") == false){ return 3; }
   else if(x.includes("u") == false && x.includes("U") == false){ return 4; }
 }
+
+// Thurs Jan 2, 2020
+// Extract domain from a url
+// JS:
+function domainName(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
+  return url.split('.')[0];
+};
