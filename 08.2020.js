8ae512e961941cc51306790bb9c369ab88c2e6ce
Matt Russo <matthew.russo91@gmail.com>
2020-08-19 10:47:35 -0400

    8/19 solution

--- 08.2020.js
+++ 08.2020.js
@@ -21,3 +21,16 @@ function getStrings(city) {
 
   return str.substring(0, str.length - 1);
 }
+
+// Aug 19
+// Running out of space (7 kyu)
+// Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing. For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace'].
+function spacey(array) {
+  let result = [];
+  let string = "";
+  array.map((word) => {
+    string += word;
+    result.push(string);
+  });
+  return result;
+}
