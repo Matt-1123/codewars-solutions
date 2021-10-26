83bb18d185f895fde87dadd35594ffded6b41196
Matt Russo <matthew.russo91@gmail.com>
2021-10-25 20:16:26 -0400

    add 8 kyu solution

--- oct2021.js
+++ oct2021.js
@@ -24,3 +24,16 @@ const reverseSeq = (n) => {
 
   return sequence;
 };
+
+// 10/25/21
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+const updateLight = (current) => {
+  if (current == "green") {
+    return "yellow";
+  } else if (current == "yellow") {
+    return "red";
+  } else if (current == "red") {
+    return "green";
+  } else console.error("Invalid input");
+};
