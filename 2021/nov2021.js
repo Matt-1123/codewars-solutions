61fce9a13fc569e0882e0e478fdac39078836ea7
Matt Russo <matthew.russo91@gmail.com>
2021-11-09 16:31:39 -0500

    organize folders into years

--- 2021/nov2021.js
+++ 2021/nov2021.js
@@ -17,3 +17,15 @@ const spongeMeme = (sentence) =>
 // Nathan drinks 0.5 litres of water per hour of cycling.
 // You are given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
 const litres = (time) => Math.floor(time / 2);
+
+// 11/9/21
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+const findDifference = (a, b) => {
+  const aVolume = a.reduce(
+    (previousValue, currentValue) => previousValue * currentValue
+  );
+  const bVolume = b.reduce(
+    (previousValue, currentValue) => previousValue * currentValue
+  );
+  return Math.abs(aVolume - bVolume);
+};
