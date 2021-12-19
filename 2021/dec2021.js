7ed4fcf7f87d6d06530cf255f6243a04db8ffdae
Matt Russo <matthew.russo91@gmail.com>
2021-12-19 07:08:21 -0500

    add solution

--- 2021/dec2021.js
+++ 2021/dec2021.js
@@ -11,22 +11,35 @@
 
 //     N = 1692. Now she has seen the digits 1, 2, 6, and 9.
 //     2N = 3384. Now she has seen the digits 1, 2, 3, 4, 6, 8, and 9.
-//     3N = 5076. Now she has seen all ten digits, and falls asleep. 
+//     3N = 5076. Now she has seen all ten digits, and falls asleep.
 
 // The purpose of this kata is to return the last number Bleatrix Trotter sees before falling asleep.
 // Input
 // Will always be positive integer or zero
 // Output
-// The last number Bleatrix Trotter sees or "INSOMNIA" (-1 in Rust, C++ and COBOL) if she will count forever 
-function trotter(n){
-  if (n === 0) 
-    return 'INSOMNIA'
-  const copy = n
-  const arr = []
-  n.toString().split('').forEach(elem => arr.includes(elem) ? undefined : arr.push(elem))
+// The last number Bleatrix Trotter sees or "INSOMNIA" (-1 in Rust, C++ and COBOL) if she will count forever
+function trotter(n) {
+  if (n === 0) return "INSOMNIA";
+  const copy = n;
+  const arr = [];
+  n.toString()
+    .split("")
+    .forEach((elem) => (arr.includes(elem) ? undefined : arr.push(elem)));
   while (arr.length !== 10) {
-    n = n + copy
-    n.toString().split('').forEach(elem => arr.includes(elem) ? undefined : arr.push(elem))
+    n = n + copy;
+    n.toString()
+      .split("")
+      .forEach((elem) => (arr.includes(elem) ? undefined : arr.push(elem)));
   }
-  return n
+  return n;
+}
+
+// 12/19/21
+// Move the zeros in an array to the end of the array, maintaining the order in which they appear in the original array.
+function removeZeros(array) {
+  let sortedArr = array.filter((num) => num != 0);
+  array.forEach((num) => {
+    if (num === 0) sortedArr.push(num);
+  });
+  return sortedArr;
 }
